9911d919a6d6c1184673c1b4e17153dd01583a35
Victoria Pavlova <viktoriya.n.pavlova@rt.ru>
2024-01-27 12:21:55 +0300

    Исправлена сигнатура в методах.

--- src/main/java/ru/pavlova/java/basic/homeworks/homework2/MainAppLesson5.java
+++ src/main/java/ru/pavlova/java/basic/homeworks/homework2/MainAppLesson5.java
@@ -4,10 +4,24 @@ import java.util.Arrays;
 
 public class MainAppLesson5 {
     public static void main(String[] args) {
-        sumArrays();
-        sumOfParts();
-        decreaseArray();
-        reverseArray();
+        int[] arr = {1, 1, 1};
+        int[] arr2 = {2, 2, 2, 2};
+        int[] arr3 = {3, 3, 3, 3, 3};
+        int[] result = sumArrays(arr, arr2, arr3);
+        System.out.println("Результат:\n" + "  " + Arrays.toString(arr));
+        System.out.println("+ " + Arrays.toString(arr2));
+        System.out.println("+ " + Arrays.toString(arr3));
+        System.out.println("= " + Arrays.toString(result) + "\n");
+
+        boolean result2 = sumOfParts(arr2);
+        System.out.println(result2 + "\n");
+
+        int[] arrDesc = {5, 1, -8, -10, 73};
+        decreaseArray(arrDesc);
+
+        int[] arrReverse = {1, 2, 3, 4};
+        int[] reversedArray = reverseArray(arrReverse);
+        System.out.println(Arrays.toString(arrReverse) + " =>\n" + Arrays.toString(reversedArray));
     }
 
     /**
@@ -15,15 +29,12 @@ public class MainAppLesson5 {
      * Реализуйте метод, принимающий на вход набор целочисленных массивов, и получающий новый
      * массив равный сумме входящих;
      * Пример: {1, 2, 3}
-     *         + {2, 2}
-     *         + {1, 1, 1, 1, 1}
-     *         = {4, 5, 4, 1, 1}
+     * + {2, 2}
+     * + {1, 1, 1, 1, 1}
+     * = {4, 5, 4, 1, 1}
      */
-    private static void sumArrays() {
-        int[] arr = {1, 1, 1};
-        int[] arr2 = {2, 2, 2, 2};
-        int[] arr3 = {3, 3, 3, 3, 3};
-        int maxSize = Integer.max(Integer.max(arr.length, arr2.length), arr3.length);
+    private static int[] sumArrays(int[] arr, int[] arr2, int[] arr3) {
+        int maxSize = Math.max(Math.max(arr.length, arr2.length), arr3.length);
         int[] sum = new int[maxSize];
         for (int i = 0; i < maxSize; i++) {
             if (i < arr.length) {
@@ -34,21 +45,16 @@ public class MainAppLesson5 {
             }
             sum[i] += arr3[i];
         }
-
-        System.out.println("Результат:\n" + "  " + Arrays.toString(arr));
-        System.out.println("+ " + Arrays.toString(arr2));
-        System.out.println("+ " + Arrays.toString(arr3));
-        System.out.println("= " + Arrays.toString(sum));
+        return sum;
     }
 
     /**
      * Задание №2
      * Реализуйте метод, проверяющий что есть “точка” в массиве, в которой сумма левой и правой части
-     *         равны. “Точка находится между элементами”;
-     *        Пример: { 1, 1, 1, 1, 1, | 5 }, { 5, | 3, 4, -2 }, { 7, 2, 2, 2 }, { 9, 4 }
+     * равны. “Точка находится между элементами”;
+     * Пример: { 1, 1, 1, 1, 1, | 5 }, { 5, | 3, 4, -2 }, { 7, 2, 2, 2 }, { 9, 4 }
      */
-    private static void sumOfParts() {
-        int[] arr = {5, 2, 1, 8};
+    private static boolean sumOfParts(int[] arr) {
         String[] strArr = new String[arr.length];
         int totalSum = 0;
         for (int i = 0; i < arr.length; i++) {
@@ -64,15 +70,21 @@ public class MainAppLesson5 {
                 strArr[i] = String.valueOf(arr[i]);
             }
         }
-        System.out.println(Arrays.toString(strArr));
+        String output = Arrays.toString(strArr).replace("|,", "|");
+        System.out.println(output);
+        for (int i = 0; i < arr.length; i++) {
+            if (strArr[i].contains("|")) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
      * Задание №3
      * Реализуйте метод, проверяющий что все элементы массива идут в порядке убывания
      */
-    private static void decreaseArray() {
-        int[] arrDesc = {5, 1, -8, -10, 73};
+    private static boolean decreaseArray(int[] arrDesc) {
         boolean arrCheck = true;
         for (int i = 1; i < arrDesc.length; i++) {
             if (arrDesc[i] >= arrDesc[i - 1]) {
@@ -81,7 +93,8 @@ public class MainAppLesson5 {
             }
         }
         System.out.println(Arrays.toString(arrDesc));
-        System.out.println(arrCheck);
+        System.out.println(arrCheck + "\n ");
+        return arrCheck;
     }
 
     /**
@@ -89,8 +102,7 @@ public class MainAppLesson5 {
      * Реализуйте метод, “переворачивающий” входящий массив
      * Пример: { 1 2 3 4 } => { 4 3 2 1 }
      */
-    private static void reverseArray() {
-        int[] arrReverse = {1, 2, 3, 4};
+    private static int[] reverseArray(int[] arrReverse) {
         int[] arrReverse2 = new int[arrReverse.length];
         for (int i = 0; i < arrReverse.length; i++) {
             arrReverse2[i] = arrReverse[i];
@@ -99,12 +111,12 @@ public class MainAppLesson5 {
         int right = arrReverse2.length - 1;
         while (left < right) {
             int temp = arrReverse2[left];
-            arrReverse2[left] = arrReverse[right];
+            arrReverse2[left] = arrReverse2[right];
             arrReverse2[right] = temp;
             left++;
             right--;
         }
-        System.out.print(Arrays.toString(arrReverse) + " =>\n" + Arrays.toString(arrReverse2));
+        return arrReverse2;
     }
 
 }
