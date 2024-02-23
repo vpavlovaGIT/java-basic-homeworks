ba8aa6e259faddd7d476b7da1d2ee70e26fe8bd8
Victoria Pavlova <viktoriya.n.pavlova@rt.ru>
2024-02-23 20:32:29 +0300

    ДЗ №14. Исключения.

---
+++ src/main/java/ru/pavlova/java/basic/homeworks/homework7/AppArrayDataException.java
@@ -0,0 +1,7 @@
+package ru.pavlova.java.basic.homeworks.homework7;
+
+public class AppArrayDataException extends Exception {
+    public AppArrayDataException(String message) {
+        super(message);
+    }
+}
