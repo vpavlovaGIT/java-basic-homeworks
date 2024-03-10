adb24f3e8a735ac6bc8c745e0ce61355548c34ff
Victoria Pavlova <viktoriya.n.pavlova@rt.ru>
2024-03-10 15:56:03 +0300

    ДЗ №18. Работа с чтением/записью файлов.

---
+++ src/main/java/ru/pavlova/java/basic/homeworks/homework11/MainAppLesson18.java
@@ -0,0 +1,46 @@
+package ru.pavlova.java.basic.homeworks.homework11;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MainAppLesson18 {
+
+    public static void main(String[] args) {
+        File rootFolder = new File(System.getProperty("user.dir"));
+        File[] files = rootFolder.listFiles();
+        String name;
+        System.out.println("Список текстовых файлов в корневом каталоге проекта:");
+        for (File file : files) {
+            if (file.exists() && file.isFile() && file.getName().endsWith(".txt")) {
+                name = file.getName();
+                System.out.println(name);
+            }
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя файла: ");
+        String fileName = scanner.nextLine();
+        File selectedFile = new File(rootFolder, fileName);
+        try {
+            if (selectedFile.exists() && selectedFile.isFile()) {
+                System.out.println("Файл " + selectedFile.getName() + " существует.");
+                System.out.println("Директория файла: " + selectedFile.getParent());
+                System.out.println("Введите строку для записи в файл:");
+                String userInput = scanner.nextLine();
+                FileWriter fileWriter = new FileWriter(selectedFile, true);
+                fileWriter.write(userInput + System.lineSeparator());
+                fileWriter.close();
+
+                System.out.println("Строка успешно записана в файл.");
+            } else {
+                System.out.println("Файл " + selectedFile.getName() + " не существует или не является файлом.");
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка при записи в файл: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
+}
